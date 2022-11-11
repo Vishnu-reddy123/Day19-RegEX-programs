6f2d8afdf0c56a17429801d83c671df262cea0a5
Vishnu-reddy123 <vishnu95532@gmail.com>
2022-11-11 19:52:18 +0530

    Added logic to validate email

---
+++ src/ValidateEmailUC3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ValidateEmailUC3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your Email:");
+        String email = scan.next();
+        String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        boolean result = email.matches(regex);
+        if(result) {
+            System.out.println("Given email-id is valid");
+        }
+        else
+        {
+            System.out.println("Given email-id is not valid");
+        }
+    }
+}
